5491bef0886884275d0729e035812a5b72d67144
Wendell <wendell.hack@gmail.com>
2013-01-04 14:25:34 +0800

    Working code

---
+++ ShoulderSurferAlert/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.Util;
+using Emgu.CV.GPU;
+
+namespace ShoulderSurferAlert {
+     /// <summary>
+     /// Interaction logic for MainWindow.xaml
+     /// </summary>
+     public partial class MainWindow : Window {
+
+          DispatcherTimer _timer; // used to refresh our image
+          Capture _capture; // this is our main capture device
+          System.Diagnostics.Stopwatch _stopwatch;
+
+
+          private const string FACEFILENAME = @"haarcascade_frontalface_default.xml";
+
+          public MainWindow() {
+               InitializeComponent();
+               this._stopwatch = new System.Diagnostics.Stopwatch();
+               // declare the timer
+               this._timer = new DispatcherTimer();
+               // tick the timer per 10 ms
+               this._timer.Interval = new TimeSpan(0, 0, 0, 0, 10);
+               // instantiate our capture device
+               this._capture = new Capture();
+
+               // initialize our timer and assign what happens every tick
+               this._timer.Tick += new EventHandler(delegate(object sender, EventArgs e) {
+                    this.DetectShoulderSurfer(); // call the worker function
+               });
+               // dispose the capture device
+               Application.Current.Exit += new ExitEventHandler(
+                    delegate(object sender, ExitEventArgs e) {
+                         if (this._capture != null) {
+                              this._capture.Dispose();
+                         }
+                    });
+               // start the fun!
+               this._timer.Start();
+          }
+
+
+          // our function to do all the work
+          protected void DetectShoulderSurfer() {
+               this._stopwatch.Reset();
+               this._stopwatch.Start();
+               // get the current frame from our webcam
+               Image<Bgr, Byte> frame = _capture.QueryFrame();
+               // push our frame to the image container
+               surferView.Source = Emgu.CV.WPF.BitmapSourceConvert.ToBitmapSource(frame);
+               //Read the HaarCascade objects
+               /*
+               using (HaarCascade face = new HaarCascade(FACEFILENAME)) {
+                    //Convert it to Grayscale
+                    using (Image<Gray, Byte> gray = frame.Convert<Gray, Byte>()) {
+                         //normalizes brightness and increases contrast of the image
+                         gray._EqualizeHist();
+                         //Detect the faces  from the gray scale image and store the locations as rectangle
+                         //The first dimensional is the channel
+                         //The second dimension is the index of the rectangle in the specific channel
+                         MCvAvgComp[] facesDetected = face.Detect(
+                              gray,
+                              1.1,
+                              10,
+                              Emgu.CV.CvEnum.HAAR_DETECTION_TYPE.DO_CANNY_PRUNING,
+                              new System.Drawing.Size(20, 20));
+                         foreach (MCvAvgComp f in facesDetected) {
+                              //draw the face detected in the 0th (gray) channel with blue color
+                              System.Diagnostics.Debug.WriteLine("Shoulder surfer detected!");
+                              //surferView.Draw(f.rect, new Bgr(Color.Blue), 2);
+                         }
+                    }
+               }
+                */
+               this._stopwatch.Stop();
+               System.Diagnostics.Debug.WriteLine("Elapsed {0}s", this._stopwatch.Elapsed.TotalSeconds);
+          }
+     }
+}
---
+++ ShoulderSurferAlertConsole/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Threading;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.Util;
+using Emgu.CV.GPU;
+using System.Drawing;
+
+namespace ShoulderSurferAlertConsole {
+     class Program : IDisposable {
+          private const string FACEFILENAME = @"haarcascade_frontalface_default.xml";
+          private System.Threading.Timer _runner;
+          private Capture _capture; // this is our main capture device
+          private System.Diagnostics.Stopwatch _stopwatch;
+
+          private System.Windows.Forms.NotifyIcon notifyIcon;
+          private System.Windows.Forms.ContextMenu contextMenu;
+          private System.Windows.Forms.MenuItem menuItem;
+          private System.ComponentModel.IContainer components;
+
+          static void Main(string[] args) {
+               using (Program execution = new Program()) {
+                    while (Console.ReadKey().Key == ConsoleKey.Enter) {
+                         break;
+                    }
+               }
+          }
+
+          public Program() {
+               bool isProcessing = false;
+               this._stopwatch = new System.Diagnostics.Stopwatch();
+               this._capture = new Capture();
+
+               this.components = new System.ComponentModel.Container();
+               this.contextMenu = new System.Windows.Forms.ContextMenu();
+               this.menuItem = new System.Windows.Forms.MenuItem();
+               this.contextMenu.MenuItems.AddRange(
+                           new System.Windows.Forms.MenuItem[] { this.menuItem });
+               this.menuItem.Index = 0;
+               this.menuItem.Text = "E&xit";
+               this.menuItem.Click += new System.EventHandler(
+                    delegate(object sender, EventArgs e) {
+                         Application.Exit();
+                    });
+               this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+               notifyIcon.Icon = new Icon("info.ico");
+               notifyIcon.ContextMenu = this.contextMenu;
+               notifyIcon.Text = "Shoulder Surfer Alert";
+               notifyIcon.Visible = true;
+               
+               this._runner = new System.Threading.Timer(new System.Threading.TimerCallback(
+                    delegate(object state) {
+                         if (!(bool)state) {
+                              state = true;
+                              this.ProcessFaces();
+                              state = false;
+                         }
+                    }), isProcessing, 0, 500);
+          }
+
+          public void ProcessFaces() {
+               this._stopwatch.Reset();
+               this._stopwatch.Start();
+               // get the current frame from our webcam
+               Image<Bgr, Byte> frame = _capture.QueryFrame();
+               Image<Gray, Byte> gray;
+               //Read the HaarCascade objects
+               using (HaarCascade face = new HaarCascade(FACEFILENAME)) {
+                    //Convert it to Grayscale
+                    gray = frame.Convert<Gray, Byte>();
+                    //normalizes brightness and increases contrast of the image
+                    gray._EqualizeHist();
+                    //Detect the faces  from the gray scale image and store the locations as rectangle
+                    //The first dimensional is the channel
+                    //The second dimension is the index of the rectangle in the specific channel
+                    MCvAvgComp[] facesDetected = face.Detect(
+                         gray,
+                         1.1,
+                         10,
+                         Emgu.CV.CvEnum.HAAR_DETECTION_TYPE.DO_CANNY_PRUNING,
+                         new System.Drawing.Size(20, 20));
+                    if (facesDetected != null && facesDetected.Length > 0) {
+                         this.notifyIcon.ShowBalloonTip(5000, "Alert!", "Shoulder surfer detected!",ToolTipIcon.Warning);
+                         Console.WriteLine("Shoulder surfer detected!");
+                    }
+
+               }
+               this._stopwatch.Stop();
+               Console.WriteLine("Elapsed {0}s", this._stopwatch.Elapsed.TotalSeconds);
+          }
+
+
+          #region IDisposable Members
+
+          public void Dispose() {
+               if (this._capture != null) {
+                    this._capture.Dispose();
+               }
+          }
+
+          #endregion
+     }
+}
